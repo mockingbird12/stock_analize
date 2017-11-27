d90efa0833da3d96da25433e4f2d66d9a3c06796
Michail <32322330+mockingbird12@users.noreply.github.com>
2017-11-27 13:28:02 -0500

    Add files via upload

--- stock_analize.py
+++ stock_analize.py
@@ -26,32 +26,38 @@ import matplotlib.pyplot as plt
 #   v подсчет всей суммы
 # 3. Статистические операции
 
+extra_index = ['MICEXINDEXCF', 'BRENT', 'USD000000TOD']
 tickers = {'GMKN':12, 'AFLT':600, 'AFKS':3700, 'MFON':120, 'YNDX':50, 'MOEX':540, 'FEES':320000, 'UPRO':17000, 'RTKM':350, 'SBERP':200}
 
 
-def plot_graf(data, name, dates):
+def plot_graf(etalon_data, cur_data, name, dates):
     dates_text = str(datetime.datetime.fromtimestamp(float(dates[0]))) + " - " + str(datetime.datetime.fromtimestamp(float(dates[1])))
-    working_array = numpy.array(data['o'])
-    max_val = [working_array.max()] * len(data['o'])
-    min_val = [working_array.min()] * len(data['o'])
+    working_array_cur = numpy.array(cur_data['o'])
+    working_array_etalon = numpy.array(etalon_data['o'])
+    max_val = [working_array_cur.max()] * len(cur_data['o'])
+    min_val = [working_array_cur.min()] * len(cur_data['o'])
+    working_array_cur = working_array_cur / working_array_cur.max()
+    working_array_etalon = working_array_etalon / working_array_etalon.max()
     plt.title(name)
-    plt.plot(data['o'])
-    plt.plot(max_val)
-    plt.plot(min_val)
-    x_text = len(data['o'])
-    y_text = float(data['o'][-1])
-    # plt.text(x_text, y_text, dates_text)
-    plt.text(len(data['o'])/2, max_val[0], max_val[0])
-    plt.text(len(data['o'])/2, min_val[0], min_val[-1])
+    plt.plot(working_array_cur.tolist())
+    plt.plot(working_array_etalon.tolist())
+    # plt.plot(max_val)
+    # plt.plot(min_val)
+    plt.text(len(cur_data['o']) / 2, max_val[0], max_val[0])
+    plt.text(len(cur_data['o']) / 2, min_val[0], min_val[-1])
     plt.xlabel(dates_text)
     plt.ylabel(u'Стоимость')
     plt.grid(True)
     plt.show()
 
 
+def plot_norm_graf(data, name, dates):
+    pass
+
+
 def set_time_interval():
     year = 2017
-    month = 8
+    month = 7
     day = 25
     hour = 10
     minute = 00
@@ -78,15 +84,15 @@ def get_current_state():
     start_date = str(set_time_interval())[:-2]
     stop_date = str(time.time())[:-3]
     dates = [start_date, stop_date]
+    micex_json = get_data_from_bcs('USD000000TOD', dates)
     for ticket in tickers.keys():
         print (ticket)
         data_json = get_data_from_bcs(ticket, dates)
-        plot_graf(data_json, ticket, dates)
+        plot_graf(micex_json, data_json, ticket, dates)
         average_annual(data_json['c'])
         dispersion(data_json['c'])
         # stock_analize(data_json)
         time.sleep(2)
-
 
 
 get_current_state()
